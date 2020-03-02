7f9c8ab041489d82029a8b91efafb0e280bd61d5
ehalfman <halfman@wisc.edu>
2020-03-02 17:51:36 -0600

    milestone 2

--- app/src/main/java/c/sakshi/lab5/NotesActivity.java
+++ app/src/main/java/c/sakshi/lab5/NotesActivity.java
@@ -6,23 +6,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
-public class NotesActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class NotesActivity extends AppCompatActivity {
+    public static ArrayList<Note> notes = new ArrayList<>();
+    private DBHelper dbHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
 
+        //Display welcome message
         TextView welcome = (TextView) findViewById(R.id.welcome);
         Intent intent = getIntent();
         String str = intent.getStringExtra("message");
         welcome.setText(str);
+
+        //get SQLiteDatabase instance
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+        dbHelper = new DBHelper(sqLiteDatabase);
+        SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+        notes = dbHelper.readNotes(username);
+        // create arraylist object iterating over notes object
+        ArrayList<String> displayNotes = new ArrayList<>();
+        for (Note note : notes){
+            displayNotes.add(String.format("Title:%s\nDate:%s", note.getTitle(), note.getDate()));
+        }
+        //use listview to display notes on screen
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, displayNotes);
+        ListView listView = (ListView) findViewById(R.id.notesListView);
+        listView.setAdapter(adapter);
+        // add onItenClickListener
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+                intent.putExtra("noteid", position);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
@@ -36,8 +71,15 @@ public class NotesActivity extends AppCompatActivity {
         Intent intent = new Intent (this, MainActivity.class);
         startActivity(intent);
     }
+
+    public void goToEditActivity(){
+        Intent intent = new Intent (this, EditActivity.class);
+        startActivity(intent);
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item){
+        super.onOptionsItemSelected(item);
+
         switch(item.getItemId()){
             case R.id.item1:
                 SharedPreferences sharedPreferences = getSharedPreferences("c.sakshi.lab5", Context.MODE_PRIVATE);
@@ -45,6 +87,7 @@ public class NotesActivity extends AppCompatActivity {
                 goToMainActivity();
                 return true;
             case R.id.item2:
+                goToEditActivity();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
